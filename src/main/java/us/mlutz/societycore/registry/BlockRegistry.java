b43c99c7949694588d6abc5e34c5f512c8eb3aa0
cazmat <me@mlutz.us>
2024-04-01 23:44:44 -0500

    Add simple placeholder block.

--- src/main/java/us/mlutz/societycore/registry/BlockRegistry.java
+++ src/main/java/us/mlutz/societycore/registry/BlockRegistry.java
@@ -1,6 +1,11 @@
 package us.mlutz.societycore.registry;
 
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -12,6 +17,11 @@ import java.util.function.Supplier;
 
 public class BlockRegistry {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Constants.ModID);
+    public static final RegistryObject<Block> PLACEHOLDER_BLOCK = register_block(
+            "placeholder_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE)),
+            CreativeModeTab.TAB_BUILDING_BLOCKS
+    );
     private static <T extends Block> RegistryObject<T> register_block(String name, Supplier<T> block, CreativeModeTab tab) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
         register_blockItem(name, toReturn, tab);

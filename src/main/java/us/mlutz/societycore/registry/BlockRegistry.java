346e55cc4a337d9afe463348b9c4c7f8b3f23d69
cazmat <me@mlutz.us>
2024-04-01 23:34:01 -0500

    Removed a coding oops.

--- src/main/java/us/mlutz/societycore/registry/BlockRegistry.java
+++ src/main/java/us/mlutz/societycore/registry/BlockRegistry.java
@@ -12,7 +12,6 @@ import java.util.function.Supplier;
 
 public class BlockRegistry {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Constants.ModID);
-    public static <T extends Block> RegistryObject<T> register_block(String name, Supplier<T> block) {
     private static <T extends Block> RegistryObject<T> register_block(String name, Supplier<T> block, CreativeModeTab tab) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
         register_blockItem(name, toReturn, tab);

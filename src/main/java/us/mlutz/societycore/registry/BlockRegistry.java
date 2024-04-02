c1e358f2ec2c1c85c0cb0e3ad82a55195ecf2465
cazmat <me@mlutz.us>
2024-04-01 23:18:42 -0500

    Add function to register blocks.

--- src/main/java/us/mlutz/societycore/registry/BlockRegistry.java
+++ src/main/java/us/mlutz/societycore/registry/BlockRegistry.java
@@ -4,11 +4,18 @@ import net.minecraft.world.level.block.Block;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import us.mlutz.societycore.Constants;
 import us.mlutz.societycore.CoreMain;
 
+import java.util.function.Supplier;
+
 public class BlockRegistry {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Constants.ModID);
+    public static <T extends Block> RegistryObject<T> register_block(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        return toReturn;
+    }
     public static void register(IEventBus iEventBus) {
         BLOCKS.register(iEventBus);
         CoreMain.logInfo("Registering " + BLOCKS.getEntries().size() + " blocks.");

83c4f8623e184f668ef96121569c8b7a3b53998b
cazmat <me@mlutz.us>
2024-04-05 01:04:58 -0500

    Started work on Death Boxes (Name TBD)

---
+++ src/main/java/us/mlutz/societycore/player/DeathEvent.java
@@ -0,0 +1,29 @@
+package us.mlutz.societycore.player;
+
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.level.GameRules;
+import net.minecraftforge.common.util.FakePlayer;
+import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.List;
+
+@Mod.EventBusSubscriber
+public class DeathEvent {
+    @SubscribeEvent
+    public static void PlayerDrops(LivingDropsEvent event) {
+        if(!(event.getEntity() instanceof ServerPlayer)) return;
+        if(event.getEntity() instanceof FakePlayer) return;
+        final List<ItemEntity> drops = (List<ItemEntity>) event.getDrops();
+        if(drops.isEmpty()) return;
+        ServerPlayer player = (ServerPlayer) event.getEntity();
+        ServerLevel level = player.getLevel();
+        GameRules gameRules = level.getGameRules();
+        if(gameRules.getBoolean(GameRules.RULE_KEEPINVENTORY)) return;
+
+        event.setCanceled(false);
+    }
+}
